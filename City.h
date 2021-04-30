4c2d8f6c27bf1204e72469e137544b3aa83ec21b
Aakash Singh <aakash301098@gmail.com>
2021-04-30 21:57:00 +0530

    final working commit

---
+++ City.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Item.h"
+
+class City
+{
+
+private:
+	string name;
+
+public:
+	Item* items[Item::NO_OF_ITEMS];
+
+	City(string _name);
+	~City();
+	void cityDetails();
+};
+
